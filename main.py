5249cfbd05a17c731bb5d3ce37cffb396354a6cf
andreishilenko <asj931@yahoo.com>
2022-12-06 16:29:37 +0300

    lala

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import random
+
+import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 from starlette.responses import JSONResponse
 from starlette.responses import FileResponse
 
 app = FastAPI()
-
+app.add_middleware(HTTPSRedirectMiddleware)
 
 @app.get("/random")
 async def generate_random_number():
@@ -16,3 +19,9 @@ async def generate_random_number():
 @app.get("/")
 async def root():
     return FileResponse('templates/index.html')
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", port=443, host='0.0.0.0', reload = True, ssl_keyfile="~/openssl/server.key",
+                ssl_certfile="~/openssl/server.crt")
+
